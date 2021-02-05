57c51174ed4da6b101d9ebfc3df70b04159e0f20
rifath33 <hrifath20@stuy.edu>
2021-02-05 13:13:29 -0500

    working on something here

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,9 @@
+// whole alphabet
+
+public static void countNoDoubleLetterWords(int length, char[] letters){
+
+    char[] characters = {};
+    print countNoDoubleLetterWords(length,"", characters);
+
+}
+
