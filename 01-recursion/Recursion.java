17100f8b5178e4573559c367e1888a73ca6dbdea
rifath33 <hrifath20@stuy.edu>
2021-02-08 18:30:56 -0500

    I added the three method signatures and I made some small progress on reverse(string s)

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,9 +1,63 @@
-// whole alphabet
-
-public static void countNoDoubleLetterWords(int length, char[] letters){
+public class Recursion{
+    
+    /*
+    *@param s any string
+    *@return a string that is the reversed version of s, do NOT use built in methods to do so, use recursion.
+    */
+    public static String reverse(String s){
+        
+        if(s.length() <= 1)
+        return s;
+        else{
+            
+        }
+        
+    }
+    
+    
+    /*
+    *@param n any non-negative value you want to take the sqrt of
+    *@return the approximate sqrt of n within a tolerance of 0.001%
+    */
+    public static double sqrt(double n){
+      //Hint: This is a wrapper method.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void countNoDoubleLetterWords(int length, char[] letters){
 
     char[] characters = {};
     print countNoDoubleLetterWords(length,"", characters);
 
+    }
+    
+    
+    
 }
-
